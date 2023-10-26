514b61ad22160a68e0155d2021f0f5a257836552
junhjuho <tmxld3857@gmail.com>
2023-10-26 20:08:57 +0900

    add dialog

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogController : MonoBehaviour
+{
+    public Text dialogText;
+    public bool isAction;
+
+    // Start is called before the first frame update
+
+    void OnEnable()
+    {
+        string sampleText = dialogText.text;
+        dialogText.text = "";
+        StartCoroutine(Typing(sampleText));
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+
+    }
+
+    IEnumerator Typing(string text)
+    {
+        foreach (char letter in text.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
