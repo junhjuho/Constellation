fd2f150ac7d9fd665730b294d962e44c7b4849bb
SHLute <133942684+SeongHyunKang@users.noreply.github.com>
2023-10-30 09:42:41 +0900

    shader error fix

---
+++ Assets/_Content/VR_Hands_FBX/Scripts/ArcheryHandAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcheryHandAnimation : MonoBehaviour {
+
+    private Animator arch_animator;
+
+	void Start ()
+    {
+        arch_animator = GetComponentInChildren<Animator>();
+	}
+	
+
+    public void TriggerGrab() => arch_animator.SetTrigger("Grab");
+    public void TriggerLet() => arch_animator.SetTrigger("Let");
+}
